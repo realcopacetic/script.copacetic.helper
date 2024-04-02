f54643dd73c09031fbbc4f64bced9408950e3ec0
realcopacetic <ahekmat400@googlemail.com>
2024-04-02 23:34:38 +0100

    remove WIP as can be done in skin

--- resources/lib/service/player.py
+++ resources/lib/service/player.py
@@ -30,42 +30,9 @@ class PlayerMonitor(Player):
         if self.isPlayingAudio():
             tag = self.getMusicInfoTag()
             user_rating = tag.getUserRating()
-            artist = tag.getArtist()
             album_artist = tag.getAlbumArtist()
             window_property('MusicPlayer_UserRating', set=user_rating)
             window_property('MusicPlayer_AlbumArtist', set=album_artist)
-
-            # Get artist multiart for visualisation background
-            filter_artist = {'field': 'artist', 'operator': 'is', 'value': artist}
-            multiart = []
-            query = json_call(
-                'AudioLibrary.GetArtists', properties=['art'], 
-                sort={'method': 'random'}, limit=15, 
-                query_filter={'and': filter_artist},
-                parent='get_visualisation_art'
-            )
-            try:
-                for result in query['result']['artist']:
-                    if result['art'].get('fanart'):
-                        data = {'title': result.get('label', '')}
-                        data.update(result['art'])
-                        multiart.append(data)
-            except KeyError:
-                pass
-            log(f'FUCK_{multiart}',force=True)
-            '''
-                    json_query = json_call('VideoLibrary.GetTVShows',
-                               properties=['title', 'lastplayed',
-                                           'studio', 'mpaa'],
-                               sort=self.sort_lastplayed, limit=25,
-                               query_filter={'and': filters},
-                               parent='next_up'
-                               )
-
-                    self.filter_actor = {'field': 'actor',
-                             'operator': 'is', 'value': self.label}
-
-            '''
 
     def onPlayBackStopped(self):
         # Clean properties
